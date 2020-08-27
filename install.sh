dfb3b2c87db7e318686e1ec69099025206c220e0
MBO <marc.bojoly@gmail.com>
2020-08-27 19:33:23 +0200

    Update install.sh
    Install terraform and cdktf

--- install.sh
+++ install.sh
@@ -3,5 +3,12 @@
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip 
 ./aws/install -i ~/aws-cli -b ~/bin
+
+wget https://releases.hashicorp.com/terraform/0.12.28/terraform_0.12.28_linux_amd64.zip
+unzip terraform_0.12.28_linux_amd64.zip
+mv terraform ~/bin/
+
+npm install --global cdktf-cli@next
+
 echo "PATH=$PATH:~/bin" >> ~/.bashrc 
 source ~/.bashrc
