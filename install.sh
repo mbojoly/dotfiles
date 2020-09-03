513991fd4412eb39233fcd3ab3f2a4720ca65680
MBO <marc.bojoly@gmail.com>
2020-09-03 16:57:16 +0200

    fix: fix coherent version of terraform and cdktf

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip 
 ./aws/install -i ~/aws-cli -b ~/bin
 
-wget https://releases.hashicorp.com/terraform/0.12.28/terraform_0.12.28_linux_amd64.zip
-unzip terraform_0.12.28_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.13.2/terraform_0.13.2_linux_amd64.zip
+unzip terraform_0.13.2_linux_amd64.zip
 mv terraform ~/bin/
 
-npm install --global cdktf-cli@next
+npm install --global cdktf-cli@0.0.16
 
 echo "PATH=$PATH:~/bin" >> ~/.bashrc 
 source ~/.bashrc
